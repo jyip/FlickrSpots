97d809b8743b7e0cc978a4f2f92611caa077e5ff
xeo <jeff_yip@me.com>
2012-07-25 16:35:57 -0700

    places displaying in first table view

--- FlickrSpots/FlickrSpotsTopPlacesViewController.m
+++ FlickrSpots/FlickrSpotsTopPlacesViewController.m
@@ -35,15 +35,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    // sort to alpha by first letter of _content
+    NSArray *sortedArray;
+    sortedArray = [[FlickrFetcher topPlaces] sortedArrayUsingComparator:^NSComparisonResult(id a, id b) {
+        NSString *first = [(NSArray*)a valueForKey:@"_content"];
+        NSString *second = [(NSArray*)b valueForKey:@"_content"];
+        return [first compare:second];
+    }];
+    self.topFlickrPlaces = sortedArray;
+    
+    //NSLog(@"%@", self.topFlickrPlaces);
 
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    self.topFlickrPlaces = [FlickrFetcher topPlaces];
-    
-    NSLog(@"%@", self.topFlickrPlaces);
 }
 
 - (void)viewDidUnload
@@ -62,24 +70,24 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return [self.topFlickrPlaces count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *CellIdentifier = @"Location Description";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
-    // Configure the cell...
+    NSString *placeInfo = [[self.topFlickrPlaces objectAtIndex:indexPath.row] valueForKey:@"_content"];
+    cell.textLabel.text = [placeInfo substringToIndex:[placeInfo rangeOfString:@", "].location];
+    cell.detailTextLabel.text = [placeInfo substringFromIndex:[placeInfo rangeOfString:@", "].location+2];
     
     return cell;
 }
