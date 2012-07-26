e1d54addc54b1b4958223bf72093609f9634e6b4
xeo <yip.jeffrey@gmail.com>
2012-07-25 19:35:18 -0700

    filename refactor

---
+++ FlickrSpots/TopPlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopPlacesViewController.h
+//  FlickrSpots
+//
+//  Created by Jeffrey Yip on 7/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesViewController : UITableViewController
+
+@end
